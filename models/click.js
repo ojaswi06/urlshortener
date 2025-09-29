7148c1957435ac498f502c33044364263e1e9970
ojaswi06 <163570429+ojaswi06@users.noreply.github.com>
2025-09-29 18:58:28 +0530

    Update click.js

--- models/click.js
+++ models/click.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 const clickSchema = new mongoose.Schema({
   urlId: { type: mongoose.Schema.Types.ObjectId, ref: "Url", required: true },
+  shortId: { type: String, required: true },
   ip: { type: String },
   userAgent: { type: String },
   timestamp: { type: Date, default: Date.now }

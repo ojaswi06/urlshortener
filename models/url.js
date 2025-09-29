6ada095edcf81c804fc00af3a514e93954a4fad0
ojaswi06 <163570429+ojaswi06@users.noreply.github.com>
2025-09-29 12:30:30 +0530

    Update url.js

--- models/url.js
+++ models/url.js
@@ -1,9 +1,10 @@
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
 
-const UrlSchema = new mongoose.Schema({
-  longUrl: { type: String, required: true },
+const urlSchema = new mongoose.Schema({
   shortId: { type: String, required: true, unique: true },
+  longUrl: { type: String, required: true },
+  clicks: { type: Number, default: 0 },
   createdAt: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model('Url', UrlSchema);
+export default mongoose.model("Url", urlSchema);
